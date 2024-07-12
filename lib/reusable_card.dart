720ea081cdb014b8875d37fb74f94bc28b401568
Shafiqullah Qasemi <shafiqullah.qsm@gmail.com>
2024-07-12 18:36:16 +0300

    Update the ReusableCard to take the onTap as argument

--- lib/reusable_card.dart
+++ lib/reusable_card.dart
@@ -3,18 +3,22 @@ import 'package:flutter/material.dart';
 class ReusableCard extends StatelessWidget {
   final Color colour;
   final Widget cardChild;
+  final void Function()? onPress;
   const ReusableCard(
-      {super.key, required this.colour, required this.cardChild});
+      {super.key, required this.colour, required this.cardChild, this.onPress});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.all(15),
-      decoration: BoxDecoration(
-        color: colour,
-        borderRadius: BorderRadius.circular(10),
+    return GestureDetector(
+      onTap: onPress,
+      child: Container(
+        margin: const EdgeInsets.all(15),
+        decoration: BoxDecoration(
+          color: colour,
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: cardChild,
       ),
-      child: cardChild,
     );
   }
 }
